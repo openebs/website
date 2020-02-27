91403d67a5a9655839a5c4ca18a9a767fa94ca1b
isamrish <askmaurya48@gmail.com>
2020-02-27 14:02:17 +0530

    Change baseURl to https://openebs.netlify.com to https://openebs.io
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: `https://openebs.netlify.com`, // Site domain. Do not include a trailing slash!
+  siteUrl: `https://openebs.io`, // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
