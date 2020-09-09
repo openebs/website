d98789e3e0b3e55efaa5ca1e664565e8e2ac0d54
isamrish <askmaurya48@gmail.com>
2020-09-09 14:27:31 +0530

    Add community meet page
    Signed-off-by: isamrish <askmaurya48@gmail.com>
    
    This commit will add community meet page.

---
+++ src/views/community-meet/community-meet.js
@@ -0,0 +1,27 @@
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+import React from 'react'
+import { Container } from '@theme-ui/components'
+import mdxPageQuery from 'utils/mdx-pagequery'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+const CommunityMeet = () => {
+  const { allMdx } = mdxPageQuery()
+  const page = allMdx.edges.find((edge) => {
+    return edge.node.frontmatter.title === `community-meet`
+  })
+
+  return (
+    <>
+      <div sx={{ height: '200px', pt: '60px', pb: '20px', bg: 'primary' }}>
+        <Styled.h2 sx={{ textAlign: 'center' }}>Making the OpenEBS Community bigger & better</Styled.h2>
+      </div>
+      <div sx={{ maxWidth: '800px', margin: 'auto', mt: '-100px', bg: 'white', padding: '16px', borderRadius: '8px' }}>
+        <Container sx={{ p: 4 }}>
+          <MDXRenderer>{page.node.body}</MDXRenderer>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default CommunityMeet
---
+++ src/views/community-meet/index.js
@@ -0,0 +1,1 @@
+export { default as CommunityMeet } from './community-meet'
