e57348418cf5146c38e9bcb03d3d80e9d1c6fb39
AVRahul <a.v.rahul11@gmail.com>
2021-06-07 19:52:15 +0530

    updated the util desc
    Signed-off-by: AVRahul <a.v.rahul11@gmail.com>

--- website/src/utils/getContent.ts
+++ website/src/utils/getContent.ts
@@ -1,4 +1,4 @@
-// Util function for fetcing the author's avatar image
+// Util function for fetcing the blog description content
 const getContent = (content: string) => {
     const blogContent = content.substring(0, 200)
     .replace(/[\n]/g, ". ")
