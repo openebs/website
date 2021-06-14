de81da7eeff86b106a31d54d9cc5bf7088bd2316
Rakesh PR <rakesh.pr@mayadata.io>
2021-06-15 03:09:59 +0530

    community events

---
+++ website/src/pages/Home/events.ts
@@ -0,0 +1,26 @@
+// Need to replace this with testimonials from GitHub 
+export const events = [
+    {
+        id: 1,
+        date: '2020-11-05',
+        title: 'Event name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu felis nec arcu eleifend scelerisque. ',
+        buttonText: 'Register',
+        buttonLink: '',
+    }, {
+        id: 1,
+        date: '2020-11-05',
+        title: 'Event name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu felis nec arcu eleifend scelerisque. ',
+    }, {
+        id: 1,
+        date: '2020-11-05',
+        title: 'Event name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu felis nec arcu eleifend scelerisque. ',
+    }, {
+        id: 1,
+        date: '2020-11-05',
+        title: 'Event name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu felis nec arcu eleifend scelerisque. ',
+    },
+];
--- website/src/pages/Home/styles.ts
+++ website/src/pages/Home/styles.ts
@@ -547,6 +547,67 @@ const useStyles = makeStyles((theme) => ({
         position: 'relative',
         bottom: 0
       },
+      imageFluid : {
+          '& img' :{
+              width: '100%'
+          }
+      },
+      slide:{
+        background: "#fff",
+        margin: "0",
+        padding: "32px",
+        boxShadow: "0px 11px 33px rgba(193, 192, 243, 0.06)",
+        borderRadius: "20px 40px 20px 0px",
+        transition: 'all 0.3s ease-out',
+      },
+      slidewrap: {
+        margin: '20px 0 20px 20px',
+        '& div.slick-active' : {
+            '& + .slick-active' : {
+                '& + .slick-slide' : {
+                    '&  > div' : {
+                        opacity: '.3',
+                    }
+                },
+            },
+        },
+        [theme.breakpoints.down('sm')]: {
+            '& div.slick-active' : {
+                '& + .slick-active' : {
+                        '&  > div' : {
+                            opacity: '.3',
+                        }
+                },
+            },
+        },
+        '&  div.slick-slide' : {
+            '&  > div' : {
+                padding: '20px'
+            }
+        },
+        '& .slick-next':{
+            right: '30%'
+        },
+        '& .slick-disabled':{
+            opacity: .5
+        },
+    },
+    titleText:{
+        fontSize: '22px',
+        fontWeight: 700,
+        color: theme.palette.primary.main,
+    },
+    subText:{
+        fontSize: '16px',
+        color: theme.palette.primary.dark
+    },
+    linkText: {
+        color: theme.palette.secondary.main,
+        fontWeight: 700,
+        '& img':{
+            marginLeft: '1rem'
+        }
+    }
 }))
 export default useStyles;
   
