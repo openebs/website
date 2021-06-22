caf9743b22468e4ffc211aca4707534addd9e5a8
Rakesh PR <rakesh.pr@mayadata.io>
2021-06-22 13:06:26 +0530

    changes on event slider
    Signed-off-by: Rakesh PR <rakesh.pr@mayadata.io>

--- website/src/pages/Home/index.tsx
+++ website/src/pages/Home/index.tsx
@@ -456,7 +456,7 @@ const Home: React.FC = () => {
                         </Grid>
                         {events.length ? (
                             <Grid item xs={12} sm={!isMobileView ? 9 : 12} xl={9}>
-                                <EventSlider />
+                                <EventSlider filterEvents={false} sortOrder="desc" />
                             </Grid>
                         ) : (
                             <Typography variant="h4" className={classes.noEventText}>
