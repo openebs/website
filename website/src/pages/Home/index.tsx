043062022d85f28e8ead4bd984a89124d1c6d0cb
Rakesh PR <rakesh.pr@mayadata.io>
2021-06-22 14:21:45 +0530

    removed unwanted params

--- website/src/pages/Home/index.tsx
+++ website/src/pages/Home/index.tsx
@@ -668,7 +668,7 @@ const Home: React.FC = () => {
                         </Grid>
                         {events.length ? (
                             <Grid item xs={12} sm={!isMobileView ? 9 : 12} xl={9}>
-                                <EventSlider filterEvents={false} sortOrder="desc" />
+                                <EventSlider />
                             </Grid>
                         ) : (
                             <Typography variant="h4" className={classes.noEventText}>
