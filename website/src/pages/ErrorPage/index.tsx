7bc8d1ecba0cdf2926cef905f6dc7587b94e76eb
A V RAHUL <a.v.rahul11@gmail.com>
2021-07-15 18:22:31 +0530

    (Feat). Error 404 page
    Signed-off-by: A V RAHUL <a.v.rahul11@gmail.com>

---
+++ website/src/pages/ErrorPage/index.tsx
@@ -0,0 +1,59 @@
+import { Button, Grid, Typography } from "@material-ui/core";
+import React from "react";
+import useStyles from "./styles";
+import { useTranslation } from "react-i18next";
+import Footer from "../../components/Footer";
+import { useHistory } from "react-router-dom";
+
+
+const ErrorPage: React.FC = () => {
+    const { t } = useTranslation();
+    const classes = useStyles();
+    const history = useHistory();
+    return (
+        <div className={classes.root}>
+            <div className={classes.wrapper}>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Grid item xs={12} lg={12}>
+                        <img
+                            loading="lazy"
+                            className={classes.errorImage}
+                            src="../images/svg/404_Image.svg"
+                            alt={t("errorPage.errorAltText")}
+                        ></img>
+                        <Typography variant="h3" className={classes.pageHeader}>
+                            {t("errorPage.description")}
+                        </Typography>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            size="large"
+                            className={classes.solidButton}
+                            onClick={() => {
+                                history.push("/");
+                            }}
+                        >
+                            {t("errorPage.homeBtnLable")}
+                        </Button>
+                    </Grid>
+                </Grid>
+                <img
+                    loading="lazy"
+                    width="100%"
+                    src="../images/svg/cloud_bottom.svg"
+                    alt={t("errorPage.cloudAltText")}
+                ></img>
+            </div>
+            {/* Display footer */}
+            <footer className={classes.footer}>
+                <Footer />
+            </footer>
+        </div >
+    );
+};
+export default React.memo(ErrorPage);
