70a5ce77f310b109ab4df2bc625953553aa743a1
isamrish <askmaurya48@gmail.com>
2021-07-21 17:47:35 +0530

    rename List to TwoColumn
    Signed-off-by: isamrish <askmaurya48@gmail.com>

--- docs/src/components/TwoColumn/index.js
+++ docs/src/components/TwoColumn/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./styles.module.scss"
-export const List = ({ children }) => {
+export const TwoColumn = ({ children }) => {
     return(
         <div className={styles.twoColumn}>
            {children}
