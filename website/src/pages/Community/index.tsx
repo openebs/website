fc181cb409220d27b342e47dfba980abf8c92353
Bala Harish <161304963+balaharish7@users.noreply.github.com>
2024-05-09 12:51:22 +0530

    community section is hidden
    Signed-off-by: Bala Harish <161304963+balaharish7@users.noreply.github.com>

--- website/src/pages/Community/index.tsx
+++ website/src/pages/Community/index.tsx
@@ -1,5 +1,5 @@
 import {
-  Button, Grid, Link, Paper, Typography,
+  Button, Grid, Paper, Typography,
 } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
